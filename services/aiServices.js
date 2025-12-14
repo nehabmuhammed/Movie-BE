fe315717127f01830fedd4dd819782ff381710fb
nehabmuhammed <128158357+nehabmuhammed@users.noreply.github.com>
2025-12-14 15:54:06 +0530

    commit

--- services/aiServices.js
+++ services/aiServices.js
@@ -10,7 +10,7 @@ module.exports.getApiResponse = async (message) => {
         {
           role: "system",
           content:
-            "You are a Movie Recommendation AI. Always return exactly 10 movie titles ONLY, separated by the symbol ' | '. No descriptions. No numbering. No newlines. No bullets. Just a single string."
+            "You are a Movie Recommendation AI. Always return exactly 10 movie titles ONLY, separated by the symbol ' | '. No descriptions. No numbering. No newlines. No bullets. Just a single string.If something asked rather than movie just reply i am a movie ai or something like that"
         },
         {
           role: "user",
